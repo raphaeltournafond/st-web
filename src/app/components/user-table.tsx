1efe1e5863855068ab18d0202224364ed2d5a9ab
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-04-29 15:54:36 +0200

    feat: connect user listing

--- src/app/components/user-table.tsx
+++ src/app/components/user-table.tsx
@@ -15,8 +15,7 @@ const UserTable: React.FC = () => {
                 await checkBackend();
                 const jsonData = await fetchUserList()
                 const userList: User[] = jsonData.map(jsonToUser);
-                console.log(userList);
-                setUserData(userList); // Update state with fetched data
+                setUserData(userList);
             } catch (error: any) {
                 console.error('Error fetching data:', error.message);
             }
@@ -33,32 +32,19 @@ const UserTable: React.FC = () => {
                 <tr>
                     <th></th>
                     <th>Name</th>
-                    <th>Level</th>
+                    <th>Email</th>
                     <th>See sessions</th>
                 </tr>
                 </thead>
                 <tbody>
-                {/* row 1 */}
-                <tr>
-                    <th>1</th>
-                    <td>Cy Ganderton</td>
-                    <td>Quality Control Specialist</td>
-                    <td>Blue</td>
-                </tr>
-                {/* row 2 */}
-                <tr>
-                    <th>2</th>
-                    <td>Hart Hagerty</td>
-                    <td>Desktop Support Technician</td>
-                    <td>Purple</td>
-                </tr>
-                {/* row 3 */}
-                <tr>
-                    <th>3</th>
-                    <td>Brice Swyre</td>
-                    <td>Tax Accountant</td>
-                    <td>Red</td>
-                </tr>
+                {userData.map((user, index) => (
+                    <tr key={index}>
+                        <th>{index + 1}</th>
+                        <td>{user.firstName} {user.lastName}</td>
+                        <td>{user.email}</td>
+                        <td>{user.id}</td>
+                    </tr>
+                ))}
                 </tbody>
             </table>
         </div>
