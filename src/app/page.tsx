b2879d07949175be589706de2a657a6f676d2096
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-04-29 13:31:39 +0200

    feat: add base code for user table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,37 @@
+import UserTable from "@/components/user-table";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">SmartTracker</a>
-      </div>
-      <div className="flex-none gap-2">
-        <div className="form-control">
-          <input type="text" placeholder="Athlete search" className="input input-bordered w-24 md:w-auto" />
+    <main>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">SmartTracker</a>
         </div>
-        <div className="dropdown dropdown-end">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              <img alt="Tailwind CSS Navbar component" src="https://cdn.pixabay.com/photo/2017/10/21/16/46/running-2875180_1280.jpg" />
-            </div>
+        <div className="flex-none gap-2">
+          <div className="form-control">
+            <input type="text" placeholder="Athlete search" className="input input-bordered w-24 md:w-auto" />
           </div>
-          <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <div className="divider"></div> 
-            <li className="bg-error rounded-xl"><a>Logout</a></li>
-          </ul>
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img alt="Tailwind CSS Navbar component" src="https://cdn.pixabay.com/photo/2017/10/21/16/46/running-2875180_1280.jpg" />
+              </div>
+            </div>
+            <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <div className="divider"></div> 
+              <li className="bg-error rounded-xl"><a>Logout</a></li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+      <UserTable title="Users"></UserTable>
+    </main>
   );
 }
