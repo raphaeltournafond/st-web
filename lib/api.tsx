9568a98c4935a24d45dd273bcc28d02974e837ce
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-04-29 13:46:25 +0200

    feat: add base api code for user fetching

---
+++ lib/api.tsx
@@ -0,0 +1,25 @@
+const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+const USER_URL = process.env.NEXT_PUBLIC_USER_URL;
+
+async function fetchAPI(url: string|undefined, method: string = 'POST', headers: any = { 'Content-Type': 'application/json' }, body: any = {}) {
+    try {
+        if (url) {
+            const response = await fetch(url, {
+                method: method,
+                headers: headers,
+                body: body
+            })
+            if (!response.ok) {
+                throw new Error(`${response.status}: ${response.url}, ${response.statusText}.`)
+            } else {
+                const data = await response.json()
+                return data
+            }
+        } else {
+            throw new Error('API URL is not set')
+        }
+    } catch (error: any) {
+        console.log(error.message)
+        throw new Error(`${error.message}.`)
+    }
+}
