9641185a080bf08373b716be9402da6cffcd5c94
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-05-01 19:27:41 +0200

    refactor: update session viewer

--- src/app/components/session-viewer.tsx
+++ src/app/components/session-viewer.tsx
@@ -1,48 +1,52 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
-import { Session } from "../types/session";
 
-interface ChartProps {
-    session: Session|undefined,
-    width: number,
-    height: number,
-    length: number,
+export interface DataLine {
+    data: number[];
+    color: string;
+    label: string;
 }
 
-const SessionViewer: React.FC<ChartProps> = ({ session, width = 600, height = 400, length = 100 }) => {
+interface ChartProps {
+    data: DataLine[];
+    width: number;
+    height: number;
+}
+
+const DataViewer: React.FC<ChartProps> = ({ data, width, height }) => {
     const svgRef = useRef<SVGSVGElement>(null);
 
     useEffect(() => {
-        if (!session) {
-            console.error('SessionViewer: Session undefined')
+        if (!data) {
+            console.error('DataViewer: Data undefined');
             return;
         }
 
         if (!svgRef.current) {
-            console.error('SessionViewer: svgRef undefined')
+            console.error('DataViewer: svgRef undefined');
             return;
         } 
 
         const svg = d3.select(svgRef.current);
 
         const margin = { top: 20, right: 30, bottom: 30, left: 50 };
-        const accelerometerRange = 40;
         const innerWidth = width - margin.left - margin.right;
         const innerHeight = height - margin.top - margin.bottom;
 
+        const maxX = Math.max(...data.map(line => line.data.length));
+        const maxY = Math.max(...data.map(line => Math.max(...line.data)));
+
         const xScale = d3.scaleLinear()
-            .domain([0, Math.min(length - 1, session.data.length - 1)])
-            .range([0, innerWidth])
+            .domain([0, maxX - 1])
+            .range([0, innerWidth]);
 
         const yScale = d3.scaleLinear()
-            .domain([-accelerometerRange, accelerometerRange])
-            .range([innerHeight, 0])
-
-        const color = d3.scaleOrdinal(d3.schemeCategory10);
+            .domain([0, maxY])
+            .range([innerHeight, 0]);
 
         const line = d3.line<number>()
             .x((d, i) => xScale(i) ?? 0)
-            .y(d => yScale(d) ?? 0)
+            .y((d) => yScale(d) ?? 0)
             .curve(d3.curveMonotoneX);
 
         const g = svg.append('g')
@@ -55,56 +59,26 @@ const SessionViewer: React.FC<ChartProps> = ({ session, width = 600, height = 40
         g.append('g')
             .call(d3.axisLeft(yScale));
 
-        // X axis
+        data.forEach((lineData) => {
+            g.append('path')
+                .datum(lineData.data)
+                .attr('fill', 'none')
+                .attr('stroke', lineData.color)
+                .attr('stroke-width', 1.5)
+                .attr('d', line);
 
-        g.append('path')
-            .datum(session.data.slice(0, length).map(d => d.x))
-            .attr('fill', 'none')
-            .attr('stroke', color('0'))
-            .attr('stroke-width', 1.5)
-            .attr('d', line);
+            g.append('text')
+                .attr('x', innerWidth)
+                .attr('y', yScale(lineData.data[lineData.data.length - 1]))
+                .attr('fill', lineData.color)
+                .text(lineData.label.toUpperCase());
+        });
 
-        g.append('text')
-            .attr('x', innerWidth)
-            .attr('y', yScale(session.data[Math.min(length - 1, session.data.length - 1)].x))
-            .attr('fill', color('0'))
-            .text('X');
-
-        // Z
-
-        g.append('path')
-            .datum(session.data.slice(0, length).map(d => d.z))
-            .attr('fill', 'none')
-            .attr('stroke', color('1'))
-            .attr('stroke-width', 1.5)
-            .attr('d', line);
-
-        g.append('text')
-            .attr('x', innerWidth)
-            .attr('y', yScale(session.data[Math.min(length - 1, session.data.length - 1)].z))
-            .attr('fill', color('1'))
-            .text('Z');
-
-        // Y
-
-        g.append('path')
-            .datum(session.data.slice(0, length).map(d => d.y))
-            .attr('fill', 'none')
-            .attr('stroke', color('2'))
-            .attr('stroke-width', 1.5)
-            .attr('d', line);
-
-        g.append('text')
-            .attr('x', innerWidth)
-            .attr('y', yScale(session.data[Math.min(length - 1, session.data.length - 1)].y))
-            .attr('fill', color('2'))
-            .text('Y');
-
-    }, [session, width, height, length]);
+    }, [data, width, height]);
 
     return (
         <svg ref={svgRef} width={width} height={height}></svg>
     );
 };
 
-export default SessionViewer;
+export default DataViewer;
