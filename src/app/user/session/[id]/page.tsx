0073d4291c395363dd792ba9fdd36eee17a0056c
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-05-02 01:21:28 +0200

    feat: add stats

--- src/app/user/session/[id]/page.tsx
+++ src/app/user/session/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { checkBackend, fetchSession } from '@/app/lib/api';
 import { Session, exportSessionData } from '@/app/types/session';
 import { jsonToSession } from '@/app/lib/utils';
@@ -9,45 +9,13 @@ import { extractStepsStats, extractPeaks, normalizeData } from '@/app/lib/proces
 
 export default function Page({ params }: { params: { id: string } }) {
     const [session, setSession] = useState<Session>();
+    const [stepCount, setStepCount] = useState<number>(0);
+    const [stepFrequency, setStepFrequency] = useState<number>(0);
+    const [stepRegularity, setStepRegularity] = useState<number>(0);
 
-    let dataLines: DataLine[] = [];
+    const length = useRef<number>(1000);
 
-    let length: number = 1000;
-
-    if (session) {
-        const [xValues, yValues, zValues] = exportSessionData(normalizeData(session.data));
-
-        dataLines.push({
-            data: xValues,
-            color: 'gray',
-            label: 'X'
-        });
-
-        dataLines.push({
-            data: yValues,
-            color: 'gray',
-            label: 'Y'
-        });
-
-        dataLines.push({
-            data: zValues,
-            color: 'gray',
-            label: 'Z'
-        });
-
-        const peaks = extractPeaks(session.data)
-        length = peaks.length * 10;
-        const { stepCount, stepFrequency, stepRegularity } = extractStepsStats(peaks);
-        console.log(stepCount);
-        console.log(stepFrequency);
-        console.log(stepRegularity);
-
-        dataLines.push({
-            data: peaks,
-            color: 'red',
-            label: 'Steps',
-        });
-    }
+    let dataLines = useRef<DataLine[]>([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -62,12 +30,79 @@ export default function Page({ params }: { params: { id: string } }) {
         fetchData();
     }, [params.id]);
 
+    useEffect(() => {
+        if (session) {
+            const [xValues, yValues, zValues] = exportSessionData(normalizeData(session.data));
+
+            dataLines.current.push({
+                data: xValues,
+                color: 'gray',
+                label: 'X'
+            });
+
+            dataLines.current.push({
+                data: yValues,
+                color: 'gray',
+                label: 'Y'
+            });
+
+            dataLines.current.push({
+                data: zValues,
+                color: 'gray',
+                label: 'Z'
+            });
+
+            const peaks = extractPeaks(session.data)
+            length.current = peaks.length * 10;
+            const { stepCount, stepFrequency, stepRegularity } = extractStepsStats(peaks);
+            setStepCount(stepCount);
+            setStepFrequency(stepFrequency);
+            setStepRegularity(stepRegularity);
+
+            dataLines.current.push({
+                data: peaks,
+                color: 'cornflowerblue',
+                label: 'Steps',
+            });
+        }
+    }, [session, dataLines]);
+
     return (
         <main className='bg-base-200 min-h-screen flex items-center justify-center'>
             <div className="container mx-auto p-4">
                 <h1 className="text-3xl font-bold text-center mb-8">Session analysis</h1>
                 <div className="bg-base-100 rounded-lg shadow-lg m-6 overflow-x-auto">
-                    <DataViewer data={dataLines} width={length} height={600} />
+                    <DataViewer data={dataLines.current} width={length.current} height={600} />
+                </div>
+                <div className="flex justify-center items-center">
+                    <div className="stats shadow stats-vertical lg:stats-horizontal">
+                        <div className="stat">
+                            <div className="stat-figure">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 640 512" className="inline-block w-8 h-8 stroke-current fill-primary"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M416 0C352.3 0 256 32 256 32V160c48 0 76 16 104 32s56 32 104 32c56.4 0 176-16 176-96S512 0 416 0zM128 96c0 35.3 28.7 64 64 64h32V32H192c-35.3 0-64 28.7-64 64zM288 512c96 0 224-48 224-128s-119.6-96-176-96c-48 0-76 16-104 32s-56 32-104 32V480s96.3 32 160 32zM0 416c0 35.3 28.7 64 64 64H96V352H64c-35.3 0-64 28.7-64 64z" /></svg>
+                            </div>
+                            <div className="stat-title">Steps</div>
+                            <div className="stat-value">{stepCount}</div>
+                            <div className="stat-desc">total count</div>
+                        </div>
+
+                        <div className="stat">
+                            <div className="stat-figure">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 640 512" className="inline-block w-8 h-8 stroke-current fill-primary"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" /></svg>
+                            </div>
+                            <div className="stat-title">Duration</div>
+                            <div className="stat-value">{stepFrequency.toFixed(2)}s</div>
+                            <div className="stat-desc">between steps</div>
+                        </div>
+
+                        <div className="stat">
+                            <div className="stat-figure">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 640 512" className="inline-block w-8 h-8 stroke-current fill-primary"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M224 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V144H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H160V320c0 17.7 14.3 32 32 32s32-14.3 32-32V208H336c17.7 0 32-14.3 32-32s-14.3-32-32-32H224V32zM0 480c0 17.7 14.3 32 32 32H352c17.7 0 32-14.3 32-32s-14.3-32-32-32H32c-17.7 0-32 14.3-32 32z" /></svg>
+                            </div>
+                            <div className="stat-title">Deviation</div>
+                            <div className="stat-value">{stepRegularity.toFixed(2)}s</div>
+                            <div className="stat-desc">average irregularity</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
