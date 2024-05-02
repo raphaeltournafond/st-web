c80630a0ee7d1622df67cee55951bbb8da635cbe
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-05-02 02:30:45 +0200

    feat: display no session yet

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -48,24 +48,30 @@ export default function Page({ params }: { params: { id: string } }) {
                             <p className='text-md'>Check out your last sessions.</p>
                         </div>
                     </div>
-                    <div className='p-6 flex flex-wrap justify-center'>
-                        {sessionData.slice().reverse().map(session => (
-                            <div key={session.id} className="card bg-base-100 shadow-xl m-6">
-                                <figure className="shadow-md">
-                                    <DataViewer data={sessionDataToDataLines(reduceDataToSize(session.data, 100))} width={300} height={200} showAxes={false} />
-                                </figure>
-                                <div className="card-body">
-                                <h2 className="card-title">{formatDate(Number(session.startDate))}</h2>
-                                <p>Duration: {formatDuration(Number(session.endDate) - Number(session.startDate))}</p>
-                                <div className="card-actions justify-end">
-                                    <button className="btn btn-primary" onClick={e => handleWatchSession(session.id.toString())}>Analyse</button>
+                    <div className='p-16 flex flex-wrap justify-center mb-48'>
+                    {sessionData.length === 0 ? (
+                        <div className="text-center w-full">
+                            <p className="text-2xl font-bold text-neutral m-10">You don&apos;t have any session recorded yet.</p>
+                        </div>
+                        ) : (
+                            sessionData.slice().reverse().map(session => (
+                                <div key={session.id} className="card bg-base-100 shadow-xl m-6">
+                                    <figure className="shadow-md">
+                                        <DataViewer data={sessionDataToDataLines(reduceDataToSize(session.data, 100))} width={300} height={200} showAxes={false} />
+                                    </figure>
+                                    <div className="card-body">
+                                        <h2 className="card-title">{formatDate(Number(session.startDate))}</h2>
+                                        <p>Duration: {formatDuration(Number(session.endDate) - Number(session.startDate))}</p>
+                                        <div className="card-actions justify-end">
+                                            <button className="btn btn-primary" onClick={e => handleWatchSession(session.id.toString())}>Analyse</button>
+                                        </div>
+                                    </div>
                                 </div>
-                                </div>
-                            </div>
-                        ))}
+                            ))
+                        )}
                     </div>
                 </div>
             </div>
         </main>
-    );
+    );    
 }
