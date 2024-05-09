a425d656422ce0c8715cd4d04d5742e2966f75f1
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-05-09 17:10:26 +0200

    feat: add 3D model viewer

---
+++ src/app/components/obj-viewer.tsx
@@ -0,0 +1,112 @@
+'use client'
+
+import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+interface OBJViewerProps {
+  objUrl: string;
+  mtlUrl: string;
+  width: number;
+  height: number;
+}
+
+const OBJViewer: React.FC<OBJViewerProps> = ({ objUrl, mtlUrl, width, height}) => {
+  const mountRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let camera: THREE.PerspectiveCamera;
+    let scene: THREE.Scene;
+    let renderer: THREE.WebGLRenderer;
+    let controls: OrbitControls;
+
+    const init = () => {
+      camera = new THREE.PerspectiveCamera(45, 1, 0.1, 20);
+      camera.position.x = 1;
+      camera.position.y = -1;
+
+      scene = new THREE.Scene();
+
+      const ambientLight = new THREE.AmbientLight(0xffffff);
+      scene.add(ambientLight);
+
+      const pointLight = new THREE.PointLight(0xffffff, 1);
+      camera.add(pointLight);
+      scene.add(camera);
+
+      const onProgress = (xhr: ProgressEvent<EventTarget>) => {
+        if (xhr.lengthComputable) {
+          const percentComplete = (xhr.loaded / xhr.total) * 100;
+          console.log(percentComplete.toFixed(2) + '% downloaded');
+        }
+      };
+
+      const mtlLoader = new MTLLoader();
+      mtlLoader.load(
+        mtlUrl,
+        (materials: MTLLoader.MaterialCreator) => {
+          materials.preload();
+
+          const objLoader = new OBJLoader();
+          objLoader.setMaterials(materials);
+          objLoader.load(
+            objUrl,
+            (object: THREE.Object3D) => {
+              object.scale.setScalar(0.01);
+              scene.add(object);
+            },
+            onProgress,
+            () => {}
+          );
+        },
+        () => {},
+        () => {}
+      );
+
+      renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true }); // Set alpha to true for transparency
+      renderer.setClearColor( 0x000000, 0 );
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(width, height);
+      if (mountRef.current) {
+        mountRef.current.appendChild(renderer.domElement);
+      }
+
+      controls = new OrbitControls(camera, renderer.domElement);
+      controls.minDistance = 0.5;
+      controls.maxDistance = 0.5;
+
+      window.addEventListener('resize', onWindowResize);
+    };
+
+    const onWindowResize = () => {
+      if (!camera) return;
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+
+      if (renderer) {
+        renderer.setSize(width, height);
+      }
+    };
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      if (renderer && scene && camera) {
+        renderer.render(scene, camera);
+      }
+    };
+
+    init();
+    animate();
+
+    return () => {
+      window.removeEventListener('resize', onWindowResize);
+    };
+  }, [objUrl, mtlUrl, width, height]);
+
+  return <div ref={mountRef} style={{ width, height }} />;
+};
+
+export default OBJViewer;
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
+import OBJViewer from './components/obj-viewer';
 
 export default function Home() {
   return (
-    <main className="bg-base-100 text-base-content flex flex-col justify-center">
-      <section className="w-1/2 m-10 p-10">
-        <div className="container mx-auto">
-          <h1 className="text-4xl font-bold mb-4">Smarter Moves, Smoother Tracks:<br/>Elevate Your Training Game 🚀</h1>
-          <p className="text-xl brightness-75 mb-8">
-          Choose AlpineTech for intelligent sports tracking.<br /><span className="border-base-content/20 border-b-2">Boost your performance</span> with state-of-the-art technology and tailored insights.
-          </p>
-          <button className="btn btn-info">Get Started</button>
+    <main className="bg-base-100 text-base-content flex flex-col justify-center text-center lg:text-left p-10">
+
+      <section className="py-20">
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+          <div>
+            <h1 className="text-4xl font-bold mb-4">Smarter Moves, Smoother Tracks:<br />Elevate Your Training Game 🚀</h1>
+            <p className="text-xl brightness-75 mb-8">
+              Choose AlpineTech for intelligent sports tracking.<br /><span className="border-base-content/20 border-b-2">Boost your performance</span> with state-of-the-art technology and tailored insights.
+            </p>
+            <button className="btn btn-info">Get Started</button>
+          </div>
+          <div className="mockup-window border bg-base-300">
+            <div className="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
+          </div>
         </div>
       </section>
 
       <section className="py-20">
         <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-          <div className="mockup-window border bg-base-300">
-            <div className="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
+          <div>
+            <OBJViewer objUrl="/smart-tracker.obj" mtlUrl="/smart-tracker.mtl" width={500} height={500} />
           </div>
           <div>
             <h2 className="text-3xl font-bold mb-4">Our Embedded Devices</h2>
