cf29a7b7b2c502160f0e5d27187459a3f7a328e3
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-06-03 14:54:56 +0200

    wip: add under construction

--- src/app/device/page.tsx
+++ src/app/device/page.tsx
@@ -2,16 +2,20 @@ import OBJViewer from "../components/obj-viewer";
 
 export default function Device() {
   return (
-    <main className="bg-base-100 text-base-content flex flex-col lg:flex-row justify-between items-center p-10">
-      <div>
-        <OBJViewer objUrl="/esp32.obj" mtlUrl="/esp32.mtl" width={300} height={300} scale={2} />
-      </div>
-      <div>
-        <OBJViewer objUrl="/ADXL345.obj" mtlUrl="/ADXL345.mtl" width={300} height={300} scale={2} />
-      </div>
-      <div>
-        <OBJViewer objUrl="/18650.obj" mtlUrl="/18650.mtl" width={300} height={300} scale={2} />
-      </div>
-    </main>
+    <div>
+      <h1 className="text-3xl p-5 bg-warning flex justify-center items-center">Under construction</h1>
+      <main className="bg-base-100 text-base-content flex flex-col xl:flex-row justify-between items-center p-10">
+        <div>
+          <OBJViewer objUrl="/esp32.obj" mtlUrl="/esp32.mtl" width={400} height={500} scale={3} />
+        </div>
+        <div>
+          <OBJViewer objUrl="/ADXL345.obj" mtlUrl="/ADXL345.mtl" width={400} height={500} scale={5} />
+        </div>
+        <div>
+          <OBJViewer objUrl="/18650.obj" mtlUrl="/18650.mtl" width={400} height={500} scale={3} />
+        </div>
+      </main>
+    </div>
+
   );
 }
