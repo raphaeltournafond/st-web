74e188db0d774adc90684c093a5d3dbc74b5208c
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-06-04 20:02:50 +0200

    style: update sizing

--- src/app/device/page.tsx
+++ src/app/device/page.tsx
@@ -6,13 +6,13 @@ export default function Device() {
       <h1 className="text-3xl p-5 bg-warning flex justify-center items-center">Under construction</h1>
       <main className="bg-base-100 text-base-content flex flex-col xl:flex-row justify-between items-center p-10">
         <div>
-          <OBJViewer objUrl="/esp32.obj" mtlUrl="/esp32.mtl" width={400} height={500} scale={3} />
+          <OBJViewer objUrl="/esp32.obj" mtlUrl="/esp32.mtl" width={400} height={500} scale={5} enableControls={false} autorotate={false} />
         </div>
         <div>
-          <OBJViewer objUrl="/ADXL345.obj" mtlUrl="/ADXL345.mtl" width={400} height={500} scale={5} />
+          <OBJViewer objUrl="/ADXL345.obj" mtlUrl="/ADXL345.mtl" width={400} height={500} scale={7} enableControls={false} autorotate={false} />
         </div>
         <div>
-          <OBJViewer objUrl="/18650.obj" mtlUrl="/18650.mtl" width={400} height={500} scale={3} />
+          <OBJViewer objUrl="/18650.obj" mtlUrl="/18650.mtl" width={400} height={500} scale={5} enableControls={false} autorotate={false} />
         </div>
       </main>
     </div>
